0154e54bde44e052c60aa305a90b54b25996b731
Mahesh <maheshmagi619@gmail.com>
2021-08-20 20:29:10 +0530

    Added pattern search feature in gistapi.py

--- tests/test_gistapi.py
+++ tests/test_gistapi.py
@@ -1,25 +1,27 @@
 import os
+import sys
 import json
 import tempfile
 
 import pytest
 
+sys.path.append('gistapi/')
 import gistapi
 
 
 @pytest.fixture
 def client(request):
-    #db_fd, gistapi.app.config['DATABASE'] = tempfile.mkstemp()
+    db_fd, gistapi.app.config['DATABASE'] = tempfile.mkstemp()
     gistapi.app.config['TESTING'] = True
     client = gistapi.app.test_client()
 
     #with gistapi.app.app_context():
     #    gistapi.init_db()
     
-    #def teardown():
-    #    os.close(db_fd)
-    #    os.unlink(flaskr.app.config['DATABASE'])
-    #request.addfinalizer(teardown)
+    def teardown():
+        os.close(db_fd)
+        os.unlink(gistapi.app.config['DATABASE'])
+    request.addfinalizer(teardown)
 
     return client
 
@@ -27,6 +29,7 @@ def client(request):
 def test_ping(client):
     """Start with a sanity check."""
     rv = client.get('/ping')
+    print('ping results', rv.data)
     assert b'pong' in rv.data
 
 
@@ -41,5 +44,16 @@ def test_search(client):
                      'username': 'justdionysus',
                      'pattern': 'TerbiumLabsChallenge_[0-9]+',
                      'matches': ['https://gist.github.com/justdionysus/6b2972aa971dd605f524']}
+    assert result_dict == expected_dict
+
+
+def test_invalid_user_name(client):
+    """ Check for invalid username tests """
+    post_data = {'username': 'invalidUsertest', 'pattern': 'TerbiumLabsChallenge_[0-9]+'}
+    rv = client.post('/api/v1/search', 
+                     data=json.dumps(post_data),
+                     headers={'content-type':'application/json'})
+    result_dict = json.loads(rv.data.decode('utf-8'))
+    expected_dict = {"message": "No user found"} 
     assert result_dict == expected_dict
 
